58aaca018ebfaf4d7e5332fe1313566bbb5c4c4c
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-12 13:00:33 -0300

    Add Tag repository

---
+++ src/main/java/br/com/zup/handora/muitosparamuitos4/repositories/TagRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.muitosparamuitos4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.muitosparamuitos4.models.Tag;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
